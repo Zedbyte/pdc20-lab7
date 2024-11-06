8c039fb0a1aed87a33cb034560410a7d8e152c68
Abr4xa$ <67954052+Zedbyte@users.noreply.github.com>
2024-11-06 17:23:19 +0800

    Add files via upload

---
+++ src/MyMainForms/MyMainForms.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import BasicForm from './BasicForms';
+import ControlledInput from './ControlledInput';
+import { Route, Routes, Link } from 'react-router-dom';
+import './MyMainForms.css';
+import UncontrolledInput from './UncontrolledInput';
+import TabbedForm from './TabForm/TabbedForm';
+
+const MyMainForms = () => {
+    return (
+        <div className="main-container">
+            {/* <h1 className="main-title">React Forms App</h1> */}
+            
+            <nav className="side-nav">
+                <ul className="nav-list">
+                    <li><Link to="basic-form" className="nav-link" >Basic Form</Link></li>
+                    <li><Link to="controlled-input" className="nav-link" >Controlled Input</Link></li>
+                    <li><Link to="uncontrolled-input" className="nav-link" >Uncontrolled Input</Link></li>
+                    <li><Link to="tabbed-form" className="nav-link" >Tabbed Form</Link></li>
+                </ul>
+            </nav>
+
+            <div className="content-area">
+                <Routes>
+                    <Route path="basic-form" element={<BasicForm />} />
+                    <Route path="controlled-input" element={<ControlledInput />} />
+                    <Route path="uncontrolled-input" element={<UncontrolledInput />} />
+                    <Route path="tabbed-form" element={<TabbedForm />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
+
+export default MyMainForms;
